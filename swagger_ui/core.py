e8c3c5f33ff67edd785edf04d888c384b58f8d47
Venkat Reddy <venkatreddyaenugu@gmail.com>
2021-07-16 18:21:44 +0530

    proxy path option added

--- swagger_ui/core.py
+++ swagger_ui/core.py
@@ -13,7 +13,8 @@ CURRENT_DIR = Path(__file__).resolve().parent
 class Interface(object):
 
     def __init__(self, app, app_type=None, config_path=None, config_url=None,
-                 url_prefix='/api/doc', title='API doc', editor=False, static_url_path='/'):
+                 url_prefix='/api/doc', title='API doc', editor=False,
+                 proxy_url_path=''):
 
         self._app = app
         self._title = title
@@ -21,7 +22,7 @@ class Interface(object):
         self._config_url = config_url
         self._config_path = config_path
         self._editor = editor
-        self._static_url_path = static_url_path
+        self._proxy_url_path = proxy_url_path
 
         assert self._config_url or self._config_path, 'config_url or config_path is required!'
 
@@ -42,7 +43,7 @@ class Interface(object):
     @property
     def doc_html(self):
         return self._env.get_template('doc.html').render(
-            url_prefix=self._url_prefix, title=self._title, config_url=self._uri('/swagger.json')
+            url_prefix=self._proxy_url_path + self._url_prefix, title=self._title, config_url=self._uri('/swagger.json')
         )
 
     @property
@@ -120,7 +121,7 @@ class Interface(object):
 
         swagger_blueprint = Blueprint(
             'swagger_blueprint', __name__, url_prefix=self._url_prefix,
-            static_folder=self.static_dir, static_url_path=self._static_url_path
+            static_folder=self.static_dir, static_url_path='/'
         )
 
         @swagger_blueprint.route(r'')
